f538bf5880533346ba2448e49c4aa0fd6a3213a3
Rakshat <rakshat2001@gmail.com>
2026-07-21 23:04:51 +0530

    deletd unwamted files

--- app/db.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
-from sqlalchemy.orm import declarative_base
-
-from config_reader import CONFIG
-
-engine = create_async_engine(CONFIG.DATABASE_URL)
-SessionLocal = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-Base = declarative_base()
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from tributaryfs!")
-
-
-if __name__ == "__main__":
-    main()
