fc3ce408b49b73c302e36993d08532fb94e3ba19
Rakshat <rakshat2001@gmail.com>
2026-07-23 20:41:02 +0530

    refactor

--- metadata_server/config/settings.py
+++ metadata_server/config/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "apps.storage",
     "apps.uploads",
     "apps.users",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
